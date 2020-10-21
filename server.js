3cdddc8088699029a0e1282762d9142b010aa397
Calvin Griffin <cgriffin332@gmail.com>
2020-10-21 10:24:33 -0400

    added pizza model

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/pizza-parlour",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
 
 app.get("/api/config", (req, res) => {
   res.json({
@@ -13,5 +34,5 @@ app.get("/api/config", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
